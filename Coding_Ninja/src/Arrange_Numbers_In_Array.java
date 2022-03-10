72b972d3489b03770decf530d55a867cd54bf88f
VIVEK DHIMAN <53940939+DHIMANvivek@users.noreply.github.com>
2022-03-10 23:41:12 +0530

    Update Arrange_Numbers_In_Array.java

--- Coding_Ninja/src/Arrange_Numbers_In_Array.java
+++ Coding_Ninja/src/Arrange_Numbers_In_Array.java
@@ -44,3 +44,25 @@ public class Arrange_Numbers_In_Array {
 		}
 	}
 }
+
+`````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````
+
+
+public class Solution {
+	public static void arrange(int[] arr, int n) {
+		int left = 0;
+		int right = n - 1;
+		int counter = 1;
+		while (left <= right) {
+			if (counter % 2 == 1) {
+				arr[left] = counter;
+				counter += 1;
+				left += 1;
+			} else {
+				arr[right] = counter;
+				counter += 1;
+				right -= 1;
+			}
+		}
+	}
+}
