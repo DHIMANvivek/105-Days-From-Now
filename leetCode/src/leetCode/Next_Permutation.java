af6fe97f0109a2da774f2f7de33d98839d0fb451
VIVEK DHIMAN <53940939+DHIMANvivek@users.noreply.github.com>
2022-03-11 23:33:39 +0530

    Update Next_Permutation.java

--- leetCode/src/leetCode/Next_Permutation.java
+++ leetCode/src/leetCode/Next_Permutation.java
@@ -1,3 +1,7 @@
+// brute 
+
+
+
 class Solution {
     public void nextPermutation(int[] A) {
         if(A == null || A.length <= 1) return;
@@ -21,4 +25,44 @@ class Solution {
     public void reverse(int[] A , int i , int j){
         while(i<j) swap(A , i++ , j--);
     }
+}
+
+
+
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+// best
+
+
+class Solution 
+{    
+    public void nextPermutation(int[] nums) 
+    {
+       int i=nums.length-2;
+        int j=0;
+        while(i>=0 && nums[i]>=nums[i+1]) i--;
+        if(i>=0)
+        {
+            j=nums.length-1;
+            while(nums[j]<=nums[i]) j--;
+            swap(nums,i,j);
+        }
+        reverse(nums,i+1,nums.length-1);
+    }
+    
+    public static void swap(int nums[],int i,int j)
+    {
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+    
+    public static void reverse(int nums[],int i,int j)
+    {
+        while(i<j)
+        {
+            swap(nums,i++,j--);
+        }
+    }
+   
 }
