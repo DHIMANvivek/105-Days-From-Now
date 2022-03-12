c1b8367f5792de461213dac0b7f6560518124b8c
VIVEK DHIMAN <53940939+DHIMANvivek@users.noreply.github.com>
2022-03-12 16:23:10 +0530

    Create Triplet_Sum.java

---
+++ Coding_Ninja/src/Triplet_Sum.java
@@ -0,0 +1,22 @@
+
+public class Solution {
+    
+    public static int findTriplet(int[] arr, int x) {
+    	//Your code goes here
+        int ans = 0;
+        for(int i = 0 ; i<arr.length ; i++){
+            for(int j = i+1 ; j< arr.length ; j++){
+                for(int k = j+1 ; k<arr.length ; k++){
+                   
+                    if(arr[i]+arr[j]+arr[k] == x){
+                       
+                    ans+=1;
+                }
+                  
+                }
+            }
+        }
+        return ans;
+    }
+
+}
