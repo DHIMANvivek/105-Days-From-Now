68b23290765265344441824878f218f9949796dd
VIVEK DHIMAN <53940939+DHIMANvivek@users.noreply.github.com>
2022-03-12 12:56:42 +0530

    Update Find_Unique.java

--- Coding_Ninja/src/Find_Unique.java
+++ Coding_Ninja/src/Find_Unique.java
@@ -22,3 +22,19 @@ public class Solution{
  
     }
 }
+
+
+```````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````
+
+public class Solution{  
+
+    public static int findUnique(int[] arr){
+	
+        int res = arr[0];
+        for (int i = 1 ; i<arr.length ; i++){
+            res = res^arr[i];
+        }
+        return res;
+ 
+    }
+}
