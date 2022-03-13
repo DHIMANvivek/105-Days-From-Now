69d0a6444b2ae436458a12e7f35f837c04cd9c75
VIVEK DHIMAN <53940939+DHIMANvivek@users.noreply.github.com>
2022-03-13 21:45:06 +0530

    Update Binary_Search.java

--- Coding_Ninja/src/Binary_Search.java
+++ Coding_Ninja/src/Binary_Search.java
@@ -1,4 +1,42 @@
 
+public class BinarySearch {
+
+	public static int binarySearch(int[] input, int ele) {
+		int start = 0;
+
+		int end = input.length - 1;
+		while (start <= end) {
+			int mid = (start + end) / 2;
+			if (ele == input[mid]) {
+				return mid;
+			} else if (ele > input[mid]) {
+				start = mid + 1;
+			} else {
+				end = mid - 1;
+			}
+		}
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		int[] input = { 2, 4, 5, 8, 19, 21, 28 };
+		int index = binarySearch(input, 19);
+		System.out.println(index);
+
+	}
+
+}
+
+
+
+`````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````
+
+
+
+
+
+
+
 public class Solution { 
 
    public static int binarySearch(int[] arr, int ele){
